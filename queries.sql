49382344999ae3966d6d6c8201d71972c4dcd842
Inna Kuts <inna.v.kuts@gmail.com>
2025-07-19 01:28:06 +0200

    task 3

--- queries.sql
+++ queries.sql
@@ -4,8 +4,23 @@
 
 -- 1. SQL-запит для перевірки кількості завантажених записів
 SELECT COUNT(*) AS total_records
-FROM infectious_cases_original; 
+FROM infectious_cases_original;
 
 -- 2. SQL-запит для перевірки кількості мігрованих записів
 SELECT COUNT(*) AS total_records
-FROM infectious_cases; 
+FROM infectious_cases;
+
+-- 3. SQL-запит для аналізу Number_rabies за Entity/Code
+SELECT 
+    e.entity,
+    e.code,
+    AVG(ic.number_rabies) AS avg_rabies,
+    MIN(ic.number_rabies) AS min_rabies,
+    MAX(ic.number_rabies) AS max_rabies,
+    SUM(ic.number_rabies) AS total_rabies
+FROM infectious_cases ic
+JOIN entities e ON ic.entity_id = e.entity_id
+WHERE ic.number_rabies IS NOT NULL
+GROUP BY e.entity, e.code
+ORDER BY avg_rabies DESC
+LIMIT 10; 
